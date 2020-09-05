b561458b69273dc59faa4e6c1d20883c7d98241a
tashfiq541 <tashfiqahmedemon1@gmail.com>
2020-09-06 03:16:15 +0600

    first redux app

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import SongList from "./SongList";
+import SongDetail from "./SongDetail";
+
+const App = () => {
+  return (
+    <div className="ui container grid">
+      <div className="eight wide column">
+        <SongList />
+      </div>
+
+      <div className="eight wide column">
+        <SongDetail />
+      </div>
+    </div>
+  );
+};
+
+export default App;
